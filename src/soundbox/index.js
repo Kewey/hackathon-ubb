dabd522e2b26e70d23e2d64bae1b95cf369e4845
Tom Siatka <47247055+tsiatka@users.noreply.github.com>
2020-10-30 10:42:09 +0100

    Update index.js

--- src/soundbox/index.js
+++ src/soundbox/index.js
@@ -25,19 +25,31 @@ class Soundbox extends Component {
 
     formSubmit(event) {
         event.preventDefault();
-        //désactive le bouton après vote
-        this.refs.btn.setAttribute("disabled", "disabled");
-        this.refs.btn.innerHTML = 'Vous avez déjà voté';
-        //ajoute le vote à firebase
-        const voteRef = db.collection('vote')
-        const vote = {
-            vote: this.state.selectedOption,
-            user: this.state.user.displayName,
+        if(this.state.selectedOption) {
+            //désactive le bouton après vote
+            this.refs.btn.setAttribute("disabled", "disabled");
+            this.refs.btn.innerHTML = 'Vous avez déjà voté';
+            //ajoute le vote à firebase
+            const voteRef = db.collection('vote')
+            const vote = {
+                vote: this.state.selectedOption,
+                user: this.state.user.displayName,
+            }
+            voteRef.doc(new Date().getTime().toString()).set(vote)
+            this.setState({
+                selectedOption: '',
+            })
+            const voteRefGet = db.collection('vote').get()
+            voteRefGet.then((querySnapshot) => {
+                const doc = querySnapshot.docs.map((doc) => {
+                    return {
+                        id: doc.id,
+                        ...doc.data()
+                    }
+            })
+                this.calcMax(doc)
+            })
         }
-        voteRef.doc(new Date().getTime().toString()).set(vote)
-        this.setState({
-            selectedOption: '',
-        })
     }
 
     componentDidMount() {
@@ -62,31 +74,48 @@ class Soundbox extends Component {
                     id: doc.id,
                     ...doc.data()
                 }
-            })
-            //récupère tous les numéros de votes
-            const voteList = doc.map(vote =>
-                (vote.vote)
-            )
-            //calcule le vote le plus fréquent
-            voteList.sort();
-            var max = 0,
-                result, freq = 0;
-            for (var i = 0; i < voteList.length; i++) {
-                if (voteList[i] === voteList[i + 1]) {
-                    freq++;
-                } else {
-                    freq = 0;
-                }
-                if (freq > max) {
-                    result = voteList[i];
-                    max = freq;
-                }
+            })         
+        this.calcMax(doc)
+        })
+    }
+
+    calcMax(doc) {
+        //récupère tous les numéros de votes
+        const voteList = doc.map(vote =>
+            (vote.vote)
+        )
+        const voteUsers = doc.map(vote =>
+            (vote.user)
+        )
+        for (var j = 0; j < voteUsers.length; j++) {
+            if (voteUsers[j] === this.state.user.displayName) {
+                this.refs.btn.setAttribute("disabled", "disabled");
+                this.refs.btn.innerHTML = 'Vous avez déjà voté';
             }
-            const maxVote = result;
-            this.setState({
-                voteList: voteList,
-                maxVote: maxVote
-            })
+        }
+
+        //calcule le vote le plus fréquent
+        voteList.sort();
+        var max = 0,
+            result, freq = 0;
+        for (var i = 0; i < voteList.length; i++) {
+            if (voteList[i] === voteList[i + 1]) {
+                freq++;
+            } else {
+                freq = 0;
+            }
+            if (freq > max) {
+                result = voteList[i];
+                max = freq;
+            }
+        }
+        if (result===undefined){
+            result = voteList[0];
+        }
+        const maxVote = result;
+        this.setState({
+            voteList: voteList,
+            maxVote: maxVote
         })
     }
     //joue l'audio quand on clique sur l'id
@@ -98,38 +127,44 @@ class Soundbox extends Component {
         //récupère la liste des sons
         const soundList = this.state.soundList.map(sound =>
             (
-            <figure value={sound.id} onClick={() => this.playAudio(sound.id)}>
-            <figcaption>{sound.label}</figcaption>
-            <audio
-                id={sound.id}
-                controls
-                src={process.env.PUBLIC_URL + sound.file}>
-                    Your browser does not support the
-                    <code>audio</code> element.
-            </audio> 
-            <input type="radio" value={sound.id} checked={this.state.selectedOption === sound.id} onChange={this.onValueChange}/>
-            </figure>
+            <>
+            <input id={sound.label} name="soundList" type="radio" value={sound.id} checked={this.state.selectedOption === sound.id} onChange={this.onValueChange}/>
+            <label htmlFor={sound.label} onClick={() => this.playAudio(sound.id)}>
+                <figure>
+                    <figcaption>{sound.label}</figcaption>
+                    <audio
+                        id={sound.id}
+                        controls
+                        src={process.env.PUBLIC_URL + sound.file}>
+                            Your browser does not support the
+                            <code>audio</code> element.
+                    </audio>            
+               </figure>
+
+            </label>                     
+
+            </>
             )
         )
         this.state.voteList.map(vote =>
             (vote.vote)
         )
-        const isEnabled = this.state.selectedOption;
         return (
         <>
-            <form onSubmit={this.formSubmit}>
-            <div className="soundList">{soundList}
+            <form style={{ backgroundImage: `url(${process.env.PUBLIC_URL +'soundbox-background.png'})`,  boxShadow: '0 0 0 200px rgba(105, 28, 51,0.3) inset',  backgroundPosition: 'center',backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}  className="soundForm" onSubmit={this.formSubmit}>
+            <h1 className="UBB-title">Mets du son au stade !</h1>
+            <div className="soundList">
+                {soundList}
             </div>
-            <div>
-            Vous avez sélectionné le bruitage numéro : {this.state.selectedOption}
-            </div>
-            <button ref="btn" disabled={!isEnabled} className="btn btn-default" type="submit">
+            <div className="soundData">
+            <p>Vous avez sélectionné le bruitage numéro : {this.state.selectedOption}</p>
+            <button ref="btn" className="btn btn-default" type="submit">
             Voter !
             </button>
-        
-            </form>
-            <p>Liste votes {this.state.voteList}</p>
             <p>Le bruitage le plus voté est le numéro : {this.state.maxVote}</p>
+            </div>
+            </form>
+            
         </>
         )
     }
